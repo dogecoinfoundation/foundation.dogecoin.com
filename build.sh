186553a102d9196ad874694deda198e08ea6812c
Florian Didron <fd@0x6664.com>
2021-08-30 13:20:16 +0900

    chore: typos

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # List all markdown files
 CONTENT_FILES=$(find content -name "*.md")
 
@@ -9,13 +8,11 @@ MASTER_FILES=$(echo "$CONTENT_FILES" | awk -F '.' '{if($2 == "md") {print $0}}')
 # Lists all the locales
 LOCALES=$(echo "$CONTENT_FILES" | awk -F '.' '{if($2 != "md") {print $2}}' | sort -u)
 
-# Sym links created are kept to be removed
-# into this array to be removed after the build process
+# Symlinks created are kept into this array to be removed after the build process
 SYM_LINKS=()
 
-# Iterates on all the master files, then iterates on all
-# the locales to check if the master file has a counterpart
-# for that locale. If not we create a symlink.
+# Iterates on all the master files, then iterates on all the locales to check if
+# the master file has a counterpart for that locale. If not we create a symlink.
 for MASTER_FILE in $MASTER_FILES
 do
   for LOCALE in $LOCALES
@@ -31,8 +28,8 @@ done
 # Run hugo's build command
 hugo --minify
 
-# Remove the symlinks we created
-# useful to avoid poluting the repo when running the command locally
+# Remove the symlinks we created useful to avoid poluting the repo when running
+# the command locally
 for SYM_LINK in ${SYM_LINKS[@]}; do
   rm $SYM_LINK
 done
