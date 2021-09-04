351241ab2f31821ecbf0fead0bcf62aaa1580c8b
Florian Didron <fd@0x6664.com>
2021-09-04 16:44:51 +0900

    feat: shrink the header when scrolling

--- themes/hello-friend-ng/assets/js/main.js
+++ themes/hello-friend-ng/assets/js/main.js
@@ -5,47 +5,61 @@
  * the theme choice of the user.
  *
  */
-const themeToggle = document.querySelector(".theme-toggle");
-const chosenTheme = window.localStorage && window.localStorage.getItem("theme");
-const chosenThemeIsDark = chosenTheme == "dark";
-const chosenThemeIsLight = chosenTheme == "light";
+(function() {
+  const themeToggle = document.querySelector(".theme-toggle");
+  const chosenTheme = window.localStorage && window.localStorage.getItem("theme");
+  const chosenThemeIsDark = chosenTheme == "dark";
+  const chosenThemeIsLight = chosenTheme == "light";
+  const header = document.getElementById("header");
 
-// Detect the color scheme the operating system prefers.
-function detectOSColorTheme() {
-  if (chosenThemeIsDark) {
-    document.documentElement.setAttribute("data-theme", "dark");
-  } else if (chosenThemeIsLight) {
-    document.documentElement.setAttribute("data-theme", "light");
-  } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-    document.documentElement.setAttribute("data-theme", "dark");
-  } else {
-    document.documentElement.setAttribute("data-theme", "light");
-  }
-}
-
-// Switch the theme.
-function switchTheme(e) {
-  if (chosenThemeIsDark) {
-    localStorage.setItem("theme", "light");
-  } else {
-    localStorage.setItem("theme", "dark");
+  // Detect the color scheme the operating system prefers.
+  function detectOSColorTheme() {
+    if (chosenThemeIsDark) {
+      document.documentElement.setAttribute("data-theme", "dark");
+    } else if (chosenThemeIsLight) {
+      document.documentElement.setAttribute("data-theme", "light");
+    } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      document.documentElement.setAttribute("data-theme", "dark");
+    } else {
+      document.documentElement.setAttribute("data-theme", "light");
+    }
   }
 
-  detectOSColorTheme();
-  window.location.reload();
-}
+  // Switch the theme.
+  function switchTheme(e) {
+    if (chosenThemeIsDark) {
+      localStorage.setItem("theme", "light");
+    } else {
+      localStorage.setItem("theme", "dark");
+    }
 
-// Event listener
-if (themeToggle) {
-  themeToggle.addEventListener("click", switchTheme, false);
-  window
-    .matchMedia("(prefers-color-scheme: dark)")
-    .addEventListener("change", (e) => e.matches && detectOSColorTheme());
-  window
-    .matchMedia("(prefers-color-scheme: light)")
-    .addEventListener("change", (e) => e.matches && detectOSColorTheme());
+    detectOSColorTheme();
+    window.location.reload();
+  }
 
-  detectOSColorTheme();
-} else {
-  localStorage.removeItem("theme");
-}
+  // Event listener
+  if (themeToggle) {
+    themeToggle.addEventListener("click", switchTheme, false);
+    window
+      .matchMedia("(prefers-color-scheme: dark)")
+      .addEventListener("change", (e) => e.matches && detectOSColorTheme());
+    window
+      .matchMedia("(prefers-color-scheme: light)")
+      .addEventListener("change", (e) => e.matches && detectOSColorTheme());
+
+    detectOSColorTheme();
+  } else {
+    localStorage.removeItem("theme");
+  }
+
+  // Shring the navbar when scrolling
+  if (header) {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 10) {
+        header.classList.add("minimize");
+      } else {
+        header.classList.remove("minimize");
+      }
+    });
+  }
+})();
